88294566a166c5d1674b9917e16f0299c1e8349e
Laurie <31747422+llamb1@users.noreply.github.com>
2023-05-22 17:58:38 -0400

    Update scripts.js
    items/exits not loading properly

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 //Scripts.js
 //Black and Blu
-//Index:
-//1. fetchItems()
-//2. fetchRooms()
-//3. fetchPeople()
-//4. populateInventory()
+
+// Fetch JSON data
 
 function fetchItems() {
   return fetch('./json/items.json')
@@ -14,50 +11,23 @@ function fetchItems() {
 }
 
 function fetchRooms() {
-    return fetch('./json/rooms.json')
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error(error))
+  return fetch('./json/rooms.json')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error(error))
 }
 
 function fetchPeople() {
-    return fetch('./json/people.json')
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error(error))
+  return fetch('./json/people.json')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.error(error))
 }
 
-fetchItems()
-  .then(data => {
-    // Process and use the fetched items data
-    console.log(data); // Log the data to the console or perform further operations
-  })
-  .catch(error => {
-    // Handle any errors that occurred during fetching
-    console.error('Error fetching items:', error);
-  });
+// Example usage: fetchItems().then(data => console.log(data)).catch(error => console.error(error));
 
-  fetchRooms()
-  .then(data => {
-    // Process and use the fetched items data
-    console.log(data); // Log the data to the console or perform further operations
-  })
-  .catch(error => {
-    // Handle any errors that occurred during fetching
-    console.error('Error fetching items:', error);
-  });
+// Inventory management
 
-  fetchPeople()
-  .then(data => {
-    // Process and use the fetched items data
-    console.log(data); // Log the data to the console or perform further operations
-  })
-  .catch(error => {
-    // Handle any errors that occurred during fetching
-    console.error('Error fetching items:', error);
-  });
-
-//Inventory management
 function populateInventory() {
   const inventoryList = document.getElementById('inventoryList');
 
@@ -106,7 +76,9 @@ function populatePeople() {
     });
 }
 
-let currentRoomID = 1;  // Initialize the room ID to 1 (or whatever room the player starts in)
+// Room navigation
+
+let currentRoomID = 1;  // Initialize the room ID to 1
 
 function fetchRoomByID(roomID) {
   return fetch('./json/rooms.json')
@@ -115,59 +87,8 @@ function fetchRoomByID(roomID) {
     .catch(error => console.error(error));
 }
 
-function updateDisplayArea() {
-    const descriptionParagraph = document.createElement('p');
-    descriptionParagraph.textContent = game.currentRoom.description;
-    
-    displayArea.innerHTML = "";
-    displayArea.appendChild(descriptionParagraph);
-    
-    // If there are additional details (like exits and items), add them to the display area.
-    if (game.currentRoom.exits) {
-        const exitsHeader = document.createElement('h2');
-        exitsHeader.textContent = "Exits";
-        displayArea.appendChild(exitsHeader);
-        
-        game.currentRoom.exits.forEach(exit => {
-            const exitParagraph = document.createElement('p');
-            exitParagraph.textContent = exit;
-            displayArea.appendChild(exitParagraph);
-        });
-    }
-    
-    if (game.currentRoom.items) {
-        const itemsHeader = document.createElement('h2');
-        itemsHeader.textContent = "Items";
-        displayArea.appendChild(itemsHeader);
-        
-        game.currentRoom.items.forEach(item => {
-            const itemParagraph = document.createElement('p');
-            itemParagraph.textContent = item.name;
-            displayArea.appendChild(itemParagraph);
-        });
-    }
-}
-
-
-
-function enterRoom(room) {
-    game.currentRoom = room;
-    
-    // Populate exits and items for the current room
-    populateExits(game.currentRoom);
-    populateItems(game.currentRoom);
-    
-    // Set the entry message as the room description
-    game.currentRoom.description = room.entryMsg;
-    
-    // Update the display area
-    updateDisplayArea();
-}
-
-
-async function populateExits() {
+async function updateDisplayArea() {
   const displayArea = document.getElementById('display-area');
-  const directions = ['up', 'down', 'north', 'south', 'east', 'west'];
 
   let room;
   try {
@@ -177,18 +98,65 @@ async function populateExits() {
     return;
   }
 
-  const exits = directions.filter(direction => room[direction] === "y");
+  let message = room.longDesc;
+  if (room.new) {
+    message = room.entryMsg;
+    room.new = false; // Mark room as visited
+  }
+
+  displayArea.innerHTML = ''; // Clear the display area
+
+  // Insert room description into the display area
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = message;
+  displayArea.appendChild(paragraph);
+
+  // Populate exits and items for the current room
+  await populateExits();
+  await populateItems();
+}
+
+async function enterRoom(room) {
+  game.currentRoom = room;
+
+  // Set the entry message as the room description
+  game.currentRoom.description = room.entryMsg;
+
+  // Update the display area
+  await updateDisplayArea();
+}
+
+async function populateExits() {
+  const exitsList = document.getElementById('exitsList');
+  exitsList.innerHTML = ''; // Clear the exits list first
+  const directions = ['Up', 'Down', 'North', 'South', 'East', 'West'];
+
+  let room;
+  try {
+    room = await fetchRoomByID(currentRoomID);
+  } catch(error) {
+    console.error('Error fetching room:', error);
+    return;
+  }
+
+  const exits = directions.filter(direction => room.exits[direction] && room.exits[direction] !== 9999);
 
   if (exits.length === 0) {
-    displayArea.insertAdjacentHTML('afterbegin', '<p>No exits</p>');
+    const listItem = document.createElement('li');
+    listItem.textContent = 'No exits';
+    exitsList.appendChild(listItem);
   } else {
-    const exitList = exits.join(', ');
-    displayArea.insertAdjacentHTML('afterbegin', `<p>Exits: ${exitList}</p>`);
+    exits.forEach(exit => {
+      const listItem = document.createElement('li');
+      listItem.textContent = exit;
+      exitsList.appendChild(listItem);
+    });
   }
 }
 
 async function populateItems() {
-  const displayArea = document.getElementById('display-area');
+  const itemsList = document.getElementById('itemsList');
+  itemsList.innerHTML = ''; // Clear the items list first
 
   let items;
   try {
@@ -199,17 +167,51 @@ async function populateItems() {
     return;
   }
 
-  const roomItems = items.filter(item => item.origRoom === currentRoomID && item.owned === "false");
+  const roomItems = items.filter(item => item.origRoom === currentRoomID && item.owned === true);
 
   if (roomItems.length === 0) {
-    displayArea.insertAdjacentHTML('afterbegin', '<p>No items</p>');
+    const listItem = document.createElement('li');
+    listItem.textContent = 'No items';
+    itemsList.appendChild(listItem);
   } else {
-    const itemList = roomItems.map(item => item.name).join(', ');
-    displayArea.insertAdjacentHTML('afterbegin', `<p>Items: ${itemList}</p>`);
+    roomItems.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.textContent = item.name;
+      itemsList.appendChild(listItem);
+    });
   }
 }
+
+
+async function move(direction) {
+  const room = await fetchRoomByID(currentRoomID);
+  const nextRoomID = room.exits[direction];
+
+  if (nextRoomID === 9999) {
+    console.log('There is no exit in that direction.');
+    return;
+  }
+
+  currentRoomID = nextRoomID;
+  await updateDisplayArea();
+  await populateExits();
+  await populateItems();
+}
+
+// Initialize the game
 
 populateInventory();
 populatePeople();
 updateDisplayArea();
 
+document.addEventListener("DOMContentLoaded", function() {
+  populateExits();
+  populateItems();
+
+  document.getElementById('move-north').addEventListener('click', () => move('North'));
+  document.getElementById('move-south').addEventListener('click', () => move('South'));
+  document.getElementById('move-east').addEventListener('click', () => move('East'));
+  document.getElementById('move-west').addEventListener('click', () => move('West'));
+  document.getElementById('move-up').addEventListener('click', () => move('Up'));
+  document.getElementById('move-down').addEventListener('click', () => move('Down'));
+});
